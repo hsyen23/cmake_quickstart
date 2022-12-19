1797ba7c9f122a0c3666ee5933993f981b491eba
hsyen23 <hsyen@ucsd.edu>
2022-12-18 17:32:08 -0800

    add Step2

---
+++ Step2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+//TODO 1: include header file to use desired functions.
+#include "myFunction.h"
+
+int main(){
+	
+	std::cout << "Hello World from main.cpp\n";
+	myFunc();
+	return 0;
+}
